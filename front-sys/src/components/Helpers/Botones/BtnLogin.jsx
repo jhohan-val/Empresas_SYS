41cf2588167df88b09f444488b4f45e2de1fe796
JuanGutiierrez <joselitoguti2003@gmail.com>
2022-12-11 17:11:10 -0500

    se elimina carpeta front

--- front-sys/src/components/Helpers/Botones/BtnLogin.jsx
+++
@@ -1,9 +0,0 @@
-const BtnLogin = () => {
-    return (
-        <section>
-                <input className="btn-iniciar" type="summit" value="Iniciar Sesión" placeholder="Iniciar Sesión"/>
-        </section>
-    )
-}
-
-export default BtnLogin;
