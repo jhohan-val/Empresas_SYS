58374c15a2a0359d05dd269d78d7d5f9781dd5fe
JuanGutiierrez <joselitoguti2003@gmail.com>
2022-12-13 11:14:09 -0500

    Se elimina toda la parte de el login porque el diseño de la pagina fue reestructurado

--- front-sys/src/components/Layouts/Main.jsx
+++ front-sys/src/components/Layouts/Main.jsx
@@ -1,4 +1,3 @@
-import Login from "../Helpers/Login";
 import Principal from "../Helpers/Principal";
 import Clientes from "../Helpers/Clientes";
 import Productos from "../Helpers/Productos";
@@ -9,21 +8,17 @@ const Main = () => {
     return (
 
         <main >
+
+        {/* ---------------------------------RUTEOS---------------------------------------- */}
             {/* <Routes> */}
-            {/*  <Route LOGIN  path="/" element={<section className="login"><Login /></section>}> */}
             {/* <Route PRINCIPAL /* path="/" element={ <Principal/> }> */}
             {/* <Route  path="/clientes" element={<Clientes />} /> */}
             {/* <Route PRODUCTOS path="/productos" element={<Productos />} /> */}
             {/* <Route PROVEEDORES path="/proveedores" element={<Proveedores />} /> */}
             {/* </Route> */}
-            {/*  </Route> */}
             {/* </Routes> */}
         {/* ------------------------------------------------------------------------------- */}
-            {/* LOGIN */}
-            {/* <section className="login">
-                    <Login/>
-                </section> */}
-
+        
             {/* PRINCIPAL */}
             {/* <Principal /> */}
 
