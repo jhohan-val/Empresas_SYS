9f69b77c205a0cdd5a1a50474021cae8290558b3
lesuduque <slduquear@cesde.net>
2022-12-13 21:41:37 -0500

    Se arregla vista principal

--- front-sys/src/components/Helpers/Header.jsx
+++ front-sys/src/components/Helpers/Header.jsx
@@ -3,17 +3,14 @@ import LogoHeader from "./imagenes/LogoHeader";
 
 const Header = () => {
   return (
-    <header className="header-menu">
-      <nav class="navbar bg-success.bg-gradient">
+    <header className="header-menu">  
         <section class="container-fluid">
           <LogoHeader />
           <a href="">Home</a>
           <a href="">Clientes</a>
           <a href="">Productos</a>
           <a href="">Proveedores</a>
-          <button class="btn-perfil" type="button" value="Salir">SALIR</button>
         </section>
-      </nav>
     </header>
   );
 };
