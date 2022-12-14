9b67e26fb804906aefb8c5447d0c8e0defded071
lesuduque <slduquear@cesde.net>
2022-12-13 22:07:48 -0500

    Se arreglan textos y botones de las cards en vista principal

--- front-sys/src/components/Helpers/imagenes/LogoHeader.jsx
+++ front-sys/src/components/Helpers/imagenes/LogoHeader.jsx
@@ -1,11 +1,11 @@
-import logoheader from '../../../../public/logo.png'
+import logoheader from '../../../../public/logo-2.png'
 
 const LogoHeader =() => {
     return(
         <section className='logo'>
             <img className="logo-header" src={logoheader} alt="logoheader"/>
         </section>
-    )
+    )   
 }
 
 export default LogoHeader
