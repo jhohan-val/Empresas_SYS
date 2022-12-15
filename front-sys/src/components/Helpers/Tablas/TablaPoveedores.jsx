c342c9752a1f5a2ceeceb8d9d30f02548661d3ba
JuanGutiierrez <joselitoguti2003@gmail.com>
2022-12-15 15:17:54 -0500

    Se importa Header y Footer en la tablaProveedores

--- front-sys/src/components/Helpers/Tablas/TablaPoveedores.jsx
+++ front-sys/src/components/Helpers/Tablas/TablaPoveedores.jsx
@@ -3,6 +3,8 @@ import BtnEditar from "../Botones/BtnEditar";
 import BtnEliminar from "../Botones/BtnEliminar";
 import axios from 'axios';
 import { useState, useEffect } from "react";
+import Header from "../Header";
+import Footer from "../Footer";
 
 const apiProveedores = "http://localhost:2002/proveedores";
 
@@ -19,37 +21,41 @@ const TablaProveedores = () => {
         console.log(response.data);
     }
     return (
-        <section className="proveedores" id="proveedores">
-            <AsideProveedores/>
-            <table className="tabla-proveedores" id="o">
-                <thead className="thead">
-                    <tr className="tr">
-                        <td className="td-proveedores">Nombre</td>
-                        <td className="td-proveedores">Nit</td>
-                        <td className="td-proveedores">Correo</td>
-                        <td className="td-proveedores">Dirección</td>
-                        <td className="td-proveedores">Teléfono</td>
-                        <td className="td-proveedores">Producto</td>
-                        <td className="td-proveedores">Acción</td>
-                    </tr>
-                </thead>
-                <tbody className="tbody">
-                    {proveedores.map((proveedores) => (
-                        <tr className="text-center" key={proveedores.id}>
-                            <td>{proveedores.nombre}</td>
-                            <td>{proveedores.nit}</td>
-                            <td>{proveedores.correo}</td>
-                            <td>{proveedores.direccion}</td>
-                            <td>{proveedores.telefono}</td>
-                            <td>{proveedores.producto}</td>
-                            <td className="d-flex">
-                            <BtnEliminar />
-                            <BtnEditar />
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+        <section>
+            <Header />
+                <section className="proveedores" id="proveedores">
+                    <AsideProveedores/>
+                    <table className="tabla-proveedores" id="o">
+                        <thead className="thead">
+                            <tr className="tr">
+                                <td className="td-proveedores">Nombre</td>
+                                <td className="td-proveedores">Nit</td>
+                                <td className="td-proveedores">Correo</td>
+                                <td className="td-proveedores">Dirección</td>
+                                <td className="td-proveedores">Teléfono</td>
+                                <td className="td-proveedores">Producto</td>
+                                <td className="td-proveedores">Acción</td>
+                            </tr>
+                        </thead>
+                        <tbody className="tbody">
+                            {proveedores.map((proveedores) => (
+                                <tr className="text-center" key={proveedores.id}>
+                                    <td>{proveedores.nombre}</td>
+                                    <td>{proveedores.nit}</td>
+                                    <td>{proveedores.correo}</td>
+                                    <td>{proveedores.direccion}</td>
+                                    <td>{proveedores.telefono}</td>
+                                    <td>{proveedores.producto}</td>
+                                    <td className="d-flex">
+                                    <BtnEliminar />
+                                    <BtnEditar />
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </section>
+            <Footer />
         </section>
     )
 }
