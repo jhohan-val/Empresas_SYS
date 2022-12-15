8b6a0e148df59c78be9757bfeed447a8256979f1
jhohan-val <valderramajhohan@gmail.com>
2022-12-15 08:45:45 -0500

    Se elimina btnAcceder

--- front-sys/src/components/Helpers/Productos.jsx
+++ front-sys/src/components/Helpers/Productos.jsx
@@ -1,12 +1,11 @@
 import AsideProductos from "./Asides/AsideProductos";
 import FormularioProductos from "./Formularios/FormularioProductos";
-import TablaProductos from "./Tablas/TablaProductos";
 
 const Productos = () =>{
     return(
         <section className="vista-productos">
-           <AsideProductos/>
-           {/* <FormularioProductos/> */}
+            <AsideProductos/>
+            <FormularioProductos/>
         </section>
     )
 }
--- front-sys/src/components/Helpers/Proveedores.jsx
+++ front-sys/src/components/Helpers/Proveedores.jsx
@@ -1,12 +1,11 @@
 import AsideProveedores from "./Asides/AsideProveedores";
 import FormularioProveedores from "./Formularios/FormularioProveedores";
-import TablaProveedores from "./Tablas/TablaPoveedores";
 
 const Proveedores = () => {
     return(
     <section className="vista-proveedores">
         <AsideProveedores/>
-        {/* <FormularioProveedores/> */}
+        <FormularioProveedores/>
         </section>
     )
 }
