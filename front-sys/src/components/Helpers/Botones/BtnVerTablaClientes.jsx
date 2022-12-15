fa0d1778f5f0f42ebc568494bb39c51a6977208a
JuanGutiierrez <joselitoguti2003@gmail.com>
2022-12-15 17:01:01 -0500

    Se crean el endPoint en btnVerTablaClientes

---
+++ front-sys/src/components/Helpers/Botones/BtnVerTablaClientes.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const BtnVerClientes = () => {
+    return(
+        <section>
+            <Link to={'/clientes/tablaClientes'} className="btn-ver" type="button" value="Ver" placeholder="Ver">Ver Clientes</Link>
+        </section>
+    )
+}
+
+export default BtnVerClientes;
