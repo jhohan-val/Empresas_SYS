ce1710fdfb28ffaaf571da96d804ed4654dfcf75
JuanGutiierrez <joselitoguti2003@gmail.com>
2022-12-15 16:55:46 -0500

    Se elimina el BtnVer y se crean botones para ver cada URI(clientes, productos, proveedores)

--- front-sys/src/components/Helpers/Botones/BtnVer.jsx
+++
@@ -1,9 +0,0 @@
-const BtnVer = () => {
-    return(
-        <section>
-            <input  className="btn-ver" type="button" value="Ver" placeholder="Ver"/>
-        </section>
-    )
-}
-
-export default BtnVer;
