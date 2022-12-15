8ad8e4d9311cf18fb5ba0ddb78f9c23a1f38a845
jhohan-val <valderramajhohan@gmail.com>
2022-12-15 10:30:58 -0500

    Se solucionan conflictos despues de hacer merge V2

--- front-sys/src/components/Helpers/Tablas/TablaPoveedores.jsx
+++ front-sys/src/components/Helpers/Tablas/TablaPoveedores.jsx
@@ -1,23 +1,14 @@
 import AsideProveedores from "../Asides/AsideProveedores";
 import BtnEditar from "../Botones/BtnEditar";
 import BtnEliminar from "../Botones/BtnEliminar";
-<<<<<<< HEAD
 import axios from 'axios';
 import { useState, useEffect } from "react";
 
 const apiProveedores = "http://localhost:2002/proveedores";
-=======
-import { useState, useEffect } from "react";
-import React from "react";
-import axios from "axios";
-
-const apiProveedores = " http://localhost:2002/proveedores";
->>>>>>> 55841c11e5fc8762f31404912098598e132a49cb
 
 const TablaProveedores = () => {
     const [proveedores, setProveedores] = useState([]);
 
-<<<<<<< HEAD
     useEffect(()=>{
         listarProveedores();
     }, []);
@@ -27,19 +18,6 @@ const TablaProveedores = () => {
         setProveedores(response.data);
         console.log(response.data);
     }
-=======
-    useEffect(() => {
-        listarProveedores();
-    }, []);
-
-    const listarProveedores = async () => {
-        const response = await axios.get(apiProveedores);
-        setProveedores(response.data);
-        console.log(response.data);
-    };
-
-
->>>>>>> 55841c11e5fc8762f31404912098598e132a49cb
     return (
         <section className="proveedores" id="proveedores">
             <AsideProveedores/>
@@ -56,22 +34,6 @@ const TablaProveedores = () => {
                     </tr>
                 </thead>
                 <tbody className="tbody">
-<<<<<<< HEAD
-                    {proveedores.map((proovedores) =>(
-                        <tr key={proovedores.id} className="tr">
-                            <td className="td-proveedores">{proovedores.nombre}</td>
-                            <td className="td-proveedores">{proovedores.nit}</td>
-                            <td className="td-proveedores">{proovedores.correo}</td>
-                            <td className="td-proveedores">{proovedores.direccion}</td>
-                            <td className="td-proveedores">{proovedores.telefono}</td>
-                            <td className="td-proveedores">{proovedores.producto}</td>
-                            <td className="d-flex">
-                                <BtnEliminar/>
-                                <BtnEditar/>
-                            </td>
-                        </tr>
-                    ))}
-=======
                 {proveedores.map((proveedores) => (
                         <tr className="text-center" key={proveedores.id}>
                             <td>{proveedores.nombre}</td>
@@ -86,7 +48,6 @@ const TablaProveedores = () => {
                         </td>
                         </tr>
                         ))}
->>>>>>> 55841c11e5fc8762f31404912098598e132a49cb
                 </tbody>
             </table>
         </section>
