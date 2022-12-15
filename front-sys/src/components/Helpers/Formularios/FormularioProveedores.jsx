98e700633fc56ce7172db72375e72716a9506116
JuanGutiierrez <joselitoguti2003@gmail.com>
2022-12-15 18:04:34 -0500

    Se comenta la libreria navigate en FormularioProveedores para que no redireccione a la URI de las tablas

--- front-sys/src/components/Helpers/Formularios/FormularioProveedores.jsx
+++ front-sys/src/components/Helpers/Formularios/FormularioProveedores.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import Footer from "../Footer";
 import Header from "../Header";
 
@@ -12,23 +12,23 @@ const FormularioProveedores = () => {
     const [correo, setCorreo] = useState("") 
     const [direccion, setDireccion] = useState("") 
     const [telefono, setTelefono] = useState("") 
-    const [producto, setProducto] = useState("") 
-    const navigate = useNavigate();
+    const [producto, setProducto] = useState("") /* 
+    const navigate = useNavigate(); */
 
-    const agregarProveedor = async(e) => {
+/*     const agregarProveedor = async(e) => {
         e.preventDefault()
         const proveedor = {
             nombre, nit, correo, direccion, telefono, producto
         }
         await axios.post(apiProveedores, proveedor)
         navigate('/proveedores/tablaProveedores')
-    }
+    } */
 
     return(
         <section>
             <Header />
                 <section className="registro">
-                    <form onSubmit={agregarProveedor} action="">
+                    <form /* onSubmit={agregarProveedor} */ action="">
                         <h1 className="titulo">Registrando proveedores</h1>
                         <input value={nombre} onChange={(e)=> setNombre(e.target.value)} className="form-item" placeholder="Nombre" type="text" />
                         <input value={nit} onChange={(e)=> setNit(e.target.value)} className="form-item" placeholder="Nit" type="text" />
@@ -36,7 +36,7 @@ const FormularioProveedores = () => {
                         <input value={direccion} onChange={(e)=> setDireccion(e.target.value)} className="form-item" placeholder="Dirección" type="text" />
                         <input value={telefono} onChange={(e)=> setTelefono(e.target.value)} className="form-item" placeholder="Teléfono" type="tel" />
                         <input value={producto} onChange={(e)=> setProducto(e.target.value)} className="form-item" placeholder="Producto" type="text" />
-                        <input className="btn-form-proveedores" type="submit" value={"Agregar"} />
+                        <Link className="btn-form-proveedores" type="submit" value={"Agregar"} />
                     </form>
                 </section>
             <Footer />
