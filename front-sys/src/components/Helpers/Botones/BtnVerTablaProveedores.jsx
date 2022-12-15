34e4c5e247c54add60c7f7e66a9d6f9d9b9801e7
JuanGutiierrez <joselitoguti2003@gmail.com>
2022-12-15 17:03:38 -0500

    Se crean el endPoint en btnVerTablaProveedores

---
+++ front-sys/src/components/Helpers/Botones/BtnVerTablaProveedores.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const BtnVerProveedores = () => {
+    return(
+        <section>
+            <Link to={'/proveedores/tablaProveedores'} className="btn-ver" type="button" value="Ver" placeholder="Ver">Ver Proveedores</Link>
+        </section>
+    )
+}
+
+export default BtnVerProveedores;
