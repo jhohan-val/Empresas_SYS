60c97997547b09a13ae316ee4000e22e92afc23d
lesuduque <slduquear@cesde.net>
2022-12-15 21:26:58 -0500

    Se agrega efecto a las cards

--- front-sys/src/components/Helpers/Principal.jsx
+++ front-sys/src/components/Helpers/Principal.jsx
@@ -10,9 +10,15 @@ const Principal = () =>{
             <Header />
                 <section className="vista-principal">
                     <section className="cards">
+                        <section className="card1">
                         <CardClientes/>
+                        </section>
+                        <section className="card2">
                         <CardProductos/>
+                        </section>
+                        <section className="card3">
                         <CardProveedores/>
+                        </section>
                     </section>
                 </section>
             <Footer />
